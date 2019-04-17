a1215d51c7aeca695c6ca9bd9eb1c8769483427b
lelopez <ux.lelopez@gmail.com>
2019-04-17 12:03:04 -0500

    Created aes-study in C

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+
+  How to compile and run on Win10:
+  > cl main.c
+  > main
+
+  How to compile and run on OSX:
+  $ clang main.c -o main
+  $ ./main
+
+ */
+
+
+
+#include <stdio.h>
+int main( int argc, const char* argv[] )
+{
+	printf( "\nHello World\n\n" );
+}
